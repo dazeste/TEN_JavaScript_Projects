9ef1e9740e1870604ee23945d1416e2f1704a257
Daina <79340025+dazeste@users.noreply.github.com>
2023-03-06 12:49:55 +0000

    Add files via upload

--- one/app.js
+++ one/app.js
@@ -3,9 +3,10 @@ const daysElements= document.getElementById("days");
 const hoursElements= document.getElementById("hours");
 const minElements= document.getElementById("min");
 const secElements= document.getElementById("sec");
+const endElements= document.getElementById("end");
 
 
-const holiday = "19 Jun 2023";
+const holiday = "23 Mar 2023";
 
 function countdown() {
   const newHolidayDate = new Date(holiday);
@@ -22,20 +23,37 @@ function countdown() {
 
   // console.log(days, hours, min, sec);
 
-  weeksElements.innerHTML = formatTime(weeks);
-  daysElements.innerHTML = formatTime(days);
-  hoursElements.innerHTML = formatTime(hours);
-  minElements.innerHTML = formatTime(min);
-  secElements.innerHTML = formatTime(sec);
+ 
+
+  weeksElements.innerHTML = formatTime(weeks) +"<span><br>weeks</span>";
+  daysElements.innerHTML = formatTime(days)+"<span><br>days</span>";
+  hoursElements.innerHTML = formatTime(hours)+"<span><br>hours</span>";
+  minElements.innerHTML = formatTime(min)+"<span><br>min</span>";
+  secElements.innerHTML = formatTime(sec)+"<span><br>sec</span>";
+
+  // Display the message when countdown is over
+  if (totalSeconds < 0) {
+    clearInterval(countdown);
+    document.getElementById("weeks").innerHTML = ""
+    document.getElementById("days").innerHTML = ""
+    document.getElementById("hours").innerHTML = "" 
+    document.getElementById("min").innerHTML = ""
+    document.getElementById("sec").innerHTML = ""
+    document.getElementById("end").innerHTML = " &#x1F389; TIME UP!!! &#x1F389;";
+  }
+    
+  
 
 }
+
 
 // craete 0
 function formatTime(time) {
   // if we have less then ten then show 0 , otherwise show time(0 front)
-  return time < 10 ? (`0${time}`) : time;
-  
+  return time < 10 ? (`0${time}`) : time;  
 }
+
+
 
 // initial call
 countdown();
