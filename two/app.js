529a7b444cf5514d43a2d79c3a437f027d336328
Daina <79340025+dazeste@users.noreply.github.com>
2023-03-06 11:29:36 +0000

    Add files via upload

---
+++ two/app.js
@@ -0,0 +1,40 @@
+const quizData = [
+  {
+    question: 'Name all five Brontë sisters?',
+    a: 'Emma,Ava, Amelia, Evelyn and Harper ',
+    b: 'Emily, Elizabeth, Charlotte, Anne and Maria',
+    c: 'Sarah, Freya, Chole, Ann and Mia',
+    d: 'Olivia, Tatiana, Dana, Sophia and Isabella',
+    correct: 'b',
+  },{
+    question: 'Which Stephen King novel takes place mostly in the fictional Overlook Hotel?',
+    a: 'The Mirror ',
+    b: 'The Curtain',
+    c: 'The Piano',
+    d: 'The Shining',
+    correct: 'd',
+
+  },{
+    question: 'What do Lord of the Rings author J.R.R. Tolkiens initials stand for?',
+    a: 'Thomas Smith',
+    b: 'Tim Pillgram',
+    c: 'John Ronald Reuel',
+    d: 'Joe Doe',
+    correct: 'c',
+  },{
+    question:'What is the best-selling novel of all time?',
+    a: 'Don Quixote',
+    b: 'A Tale of Two Cities',
+    c: 'The Lord of the Rings ',
+    d: 'The Little Prince',
+    correct: 'a',
+
+  },{
+    question:'Who authored The Hunger Games book series?',
+    a: 'Charles Dickens',
+    b: 'Suzanne Collins',
+    c: 'Fyodor Dostoevsky',
+    d: 'William Shakespeare',
+    correct: 'b',
+  }
+]
