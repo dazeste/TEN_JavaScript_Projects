32d8a4a660e7c735ee20a9a85cee9f5dbd631067
Daina <79340025+dazeste@users.noreply.github.com>
2023-03-24 20:08:37 +0000

    Add files via upload

--- three/app.js
+++ three/app.js
@@ -1,11 +1,19 @@
+getRandomMeal();
+
 async function getRandomMeal() {
-    const randomMeal = fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-}
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+    const randomMeal = await resp.json();
+    console.log(randomMeal)
+};
+
+
+
 
 async function getMealById(id) {
-   const mealById = fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" +id);
+   const mealById = await  fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" +id);
 }
 
 async function getMealsBySearch(term) {
-    const mealsBySearch = fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" +term);
+    const mealsBySearch = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" +term);
 }
